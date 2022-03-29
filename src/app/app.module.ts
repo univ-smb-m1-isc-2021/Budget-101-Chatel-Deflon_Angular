fd74f896b4e6a8a630aa865062f8427f0d54b57a
Elodie D <elodie.deflon@gmail.com>
2022-03-29 09:08:23 +0200

    Update homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,15 @@ import { HomepageComponent } from './components/homepage/homepage.component';
 import { HttpClientModule } from "@angular/common/http";
 import {MatCardModule} from "@angular/material/card";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatListModule} from "@angular/material/list";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatRadioModule} from "@angular/material/radio";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [
@@ -18,11 +27,23 @@ import {MatToolbarModule} from "@angular/material/toolbar";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
+    MatButtonModule,
     MatCardModule,
+    MatFormFieldModule,
     MatToolbarModule,
+    MatSelectModule,
+    MatInputModule,
+    MatListModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatTableModule,
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {floatLabel: 'always'}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiService} from "../../services/api.service";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-homepage',
@@ -9,10 +10,20 @@ import {ApiService} from "../../services/api.service";
 export class HomepageComponent implements OnInit {
   // todos: Todo[] = [];
   public hello: string[] = [];
+  options: FormGroup;
+  typeValue = new FormControl(false);
+  freqValue = new FormControl('auto');
+  selectFormControl = new FormControl('');
 
   constructor(
-    private apiService: ApiService
-  ) { }
+    private apiService: ApiService,
+    fb: FormBuilder
+  ) {
+    this.options = fb.group({
+      typeValue: this.typeValue,
+      freqValue: this.freqValue,
+    });
+  }
 
   ngOnInit(): void {
     // this.apiService.getTodos().subscribe(todos => {
@@ -22,6 +33,10 @@ export class HomepageComponent implements OnInit {
     this.apiService.getHello().subscribe(data => {
       this.hello = data;
     })
+  }
+
+  Submit() {
+    console.log("test submit");
   }
 
   // removeTodos(): void {
